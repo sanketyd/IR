de9d535a6e8d6e0f53899a2c649872fcd3fd7ac3
Sanit <ankitsalvi233@gmail.com>
2018-03-24 14:21:36 +0530

    Uptil step 7 completed

--- PLIS.java
+++ PLIS.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.Vector;
 import java.awt.ComponentOrientation;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,9 +16,11 @@ import java.util.stream.Collectors;
 
 public class PLIS 
 {
-	 static Set<String> gamma = new HashSet<String>();
+	static Vector gamma = new Vector();
+
 	public static void main(String[] args) throws IOException 
 	{
+		
 		int flag=0;
 	//Query parsing
 		
@@ -30,7 +33,7 @@ public class PLIS
 	Path path =  Paths.get("/home/ankit/Downloads/PLIS/PLIS/src/Stopwords").toAbsolutePath();
 	List<String> list = Files.lines(path).collect(Collectors.toList());	
 	
-	String word = "ىشقعففخ";
+	String word = "Narutto";
 
 	
 	boolean inFile = list.stream().anyMatch(p->p.equalsIgnoreCase(word));
@@ -54,38 +57,274 @@ public class PLIS
 		}
 		else 
 		{
+			
 			String three_letter;
 			
 			//Step 4
 			
-//			char[] chars = word.toCharArray();
-//			for(char c: chars){
-//			    if(c >= 0x600 && c <= 0x6ff){
-//			    	System.out.println("yolo");
-//			        flag=1;
-//			        System.out.println("yolo2");
-//			        break;
-//			     }
-//			}
+
 			
-/*			if(flag==1) 
-			{
-				;
-				three_letter = word.substring(length-3,length);
-				
-			}
-			else 
-			{
-				
-*/				three_letter = word.substring(0,3);
-//			}
+			three_letter = word.substring(0,3);
+				System.out.println(three_letter);
+
 			
-//			String s= "مورومومرممورمو";
+
 			
 			
 
 			return_words_from_document(three_letter, length);
+			int gamma_size = gamma.size();
+		
 			System.out.println(gamma);
+			System.out.println(((String) gamma.get(0)).length());
+			
+			
+			//Step 5
+			
+		int n = word.length();
+		int ED[] = new int [gamma_size];
+		int index = 0;
+		for(int k=0;k<gamma.size();k++)
+		{	
+			
+			String element = (String)gamma.get(k);
+			System.out.println(element);
+			int m = element.length();
+			System.out.println(m);
+			int [][] mat = new int [n+1][m+1];
+			
+			
+			for(int i=0;i< n+1;i++) 
+			{
+				
+				for(int j=0;j< m+1;j++)
+				{
+					if(i==0 && j==0) 
+					{
+						mat[i][j]=0;
+					}
+					
+					if(i==0 && j!=0) 
+					{
+						mat[i][j]=j;
+					}
+					
+					if(j==0 && i!=0) 
+					{
+						mat[i][j]=i;
+					}
+				}
+			}
+			
+			
+			for(int i=1;i<n+1;i++) 
+			{
+				
+				for(int j=1;j<m+1;j++)
+				{
+					
+					
+		
+					
+					if(word.charAt(i-1)==element.charAt(j-1) )
+					{
+						
+						
+						mat[i][j]=Math.min(Math.min(mat[i-1][j]+1, mat[i][j-1]+1),mat[i-1][j-1]);
+						
+					}
+					
+					
+					if(word.charAt(i-1)!=element.charAt(j-1) ) 
+					{
+						
+						mat[i][j]=Math.min(Math.min(mat[i-1][j]+1, mat[i][j-1]+1),mat[i-1][j-1]+2);
+					
+					}
+					
+				
+				}
+			
+			}
+			
+			ED[index]=mat[n][m];
+			System.out.println("ED");
+			System.out.println(ED[index]);
+			
+			index = index+1;
+			
+		
+		}
+		
+		
+		
+		
+		
+		int LCS[] = new int [gamma_size];
+		index = 0;
+		for(int k=0;k<gamma.size();k++)
+		{	
+			
+			String element = (String)gamma.get(k);
+			int m = element.length();
+			int [][] mat = new int [n+1][m+1];
+			
+			
+			for(int i=0;i< n+1;i++) 
+			{
+				
+				for(int j=0;j< m+1;j++)
+				{
+					if(i==0) 
+					{
+						mat[i][j]=0;
+					}
+					if(j==0) 
+					{
+						mat[i][j]=0;
+					}
+				}
+			}
+			
+			
+			
+			
+			
+			for(int i=1;i< n+1;i++) 
+			{
+				
+				for(int j=1;j< m+1;j++)
+				{
+					
+					
+					if(i==0) 
+					{
+						mat[i][j]=0;
+					}
+					if(j==0) 
+					{
+						mat[i][j]=0;
+					}
+					
+					if(word.charAt(i-1)==element.charAt(j-1))
+					{
+						mat[i][j]=mat[i-1][j-1]+1;
+					}
+				
+					
+					if(word.charAt(i-1)!=element.charAt(j-1)) 
+					{
+						mat[i][j]=Math.max(mat[i-1][j], mat[i][j-1]);
+					}
+				
+				
+				}
+			
+			}
+			LCS[index]=mat[n][m];
+			index = index+1;
+		
+		}
+		
+		//Step 6 and Step 7
+		Vector vec1 = new Vector();
+		Vector ED1 = new Vector();
+		
+		Vector LCS1 = new Vector();
+		
+		
+		
+		for(int i=0;i<gamma_size;i++) 
+		{
+			
+			if(ED[i]+LCS[i]==word.length() && ED[i] < LCS[i]) 
+			{
+				System.out.println("ntoken4");
+				vec1.add((String)gamma.get(i));
+				ED1.add((int)ED[i]);
+				LCS1.add((int)LCS[i]);
+			}
+			
+		}
+		
+	
+		int maxED1=(int)ED1.get(0);
+		System.out.println("ntoken5");
+		for(int i=1;i<gamma_size;i++) 
+		{
+			
+			if((int)ED1.get(i)>maxED1) 
+			{
+				maxED1=(int)ED1.get(i);
+			}
+		}
+			
+			
+		int minLCS1=(int)LCS1.get(0);
+		for(int i=0;i<gamma_size;i++) 
+		{
+			if((int)LCS1.get(i)>minLCS1) 
+			{
+				minLCS1=(int)LCS1.get(i);
+			}
+		}
+		
+		Vector gamma_star = new Vector();
+		for(int i=0;i<gamma_size;i++) 
+		{
+			if((int)ED1.get(i)==maxED1 && (int)LCS1.get(i)==minLCS1) 
+			{
+				gamma_star.add((String)gamma.get(i));
+			}
+		}
+		
+		
+		if(gamma_star.size()==1) 
+		{
+			//return the single word as stem word and goto step 8
+		}
+		int min_len_index;
+		int count =0;
+		if(gamma_star.size()>1) 
+		{
+			int min_len=((String) gamma_star.get(0)).length();
+			for(int i=0;i<gamma_star.size();i++) 
+			{
+				if(((String) gamma_star.get(i)).length()<min_len) 
+				{
+					min_len=((String) gamma_star.get(i)).length();
+					min_len_index=i;
+				}
+			}
+		
+			for(int i=0;i<gamma_star.size();i++) 
+			{
+				if(((String) gamma_star.get(i)).length()==min_len) 
+				{
+					count=count+1;
+				}
+			}
+			
+			if(count > 1) 
+			{
+				//return most similar word and goto step 8
+			}
+			
+			
+			
+			//return gamma_star.get(min_len_index) and goto step 8
+			
+		}
+	
+	
+		
+		
+		
+			
+			
+			
+
+			
 		}
 	}
 		
@@ -104,17 +343,10 @@ public class PLIS
 		{	
 			
 		    String nextToken = scanner.next();
-		    //System.out.println(nextToken);
+		  
     	    String three_letter;
-/*		    if(flag==1) 
-			{
-		    
-				three_letter = nextToken.substring(length-3,length);
-		}
-			else 
-			{
-*/				three_letter = nextToken.substring(0, 3);
-//			}
+				three_letter = nextToken.substring(0, 3);
+
 		    
 		    
 		    
